f1093c064a264066840dcab47523045a223aa90d
tomoya <shima.apple0941@gmail.com>
2022-12-14 23:46:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route, Link, useParams } from "react-router-dom";
 
-function App() {
+const App = () => (
+  <BrowserRouter>
+    <div className="container mt-5 text-center">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog/:id" element={<Blog />} />
+      </Routes>
+    </div>
+  </BrowserRouter>
+);
+
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome!</h1>
+      <p><Link to="/about">About</Link></p>
     </div>
   );
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p><Link to="/">Home</Link></p>
+    </div>
+  )
+}
+
+const Blog = () => {
+  const params = useParams();
+  const id = params.id;
+
+  return (
+    <div>
+      <p>{id}番目の記事です</p>
+    </div>
+  )
 }
 
 export default App;
